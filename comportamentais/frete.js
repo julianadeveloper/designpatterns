59d62ee71a24dff92563d7e149d0492b6c49fb95
julianafirmiano <juliana.firmiano@rdstation.com>
2025-06-28 12:46:18 -0300

    test 2

--- comportamentais/frete.js
+++
@@ -1,7 +0,0 @@
-class Frete {
-    calcula(valorPedido) {
-        throw new Error("Method 'calcula(valorPedido)' must be implemented.");
-    }
-}
-
-export default Frete
--- comportamentais/freteExpresso.js
+++
@@ -1,8 +0,0 @@
-import Frete from "./frete.js";
-class FreteExpresso extends Frete {
-    calcula(valor) {
-      return valor * 0.2;
-    }
-  
-}
-export default FreteExpresso;
